e9a84bd0ec75cdd6b10ef349e2d9b1e58e0f7ad3
Chris Henke <chris@brittonmdg.com>
2018-01-22 14:40:47 -0500

    Removing logging of account details.

--- src/components/SocialAccount.js
+++ src/components/SocialAccount.js
@@ -4,7 +4,6 @@ class SocialAccount extends React.Component{
     render(){
         return (
         <li>
-            {console.log(this.props)}
             <a title={this.props.details.name} href={this.props.details.link} target="_blank">
               <i className={`fa ${this.props.details.icon}`}></i>
             </a>
